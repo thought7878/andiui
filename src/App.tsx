10053fd912a1c63f013235be364b4951272a9b68
Calm Andy <thought7878@qq.com>
2023-01-17 14:52:57 +0800

    create: css basic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	return <div className="App">hello,aui</div>;
+	return (
+		<div className="App">
+			<div>hello,aui</div>
+			<h2>hello,aui</h2>
+			<h3>hello,aui</h3>
+			<h4>hello,aui</h4>
+			<h5>hello,aui</h5>
+			<code>var a=1;</code>
+		</div>
+	);
 }
 
 export default App;
