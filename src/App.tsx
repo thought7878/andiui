9ee03e5c27c1a1d9434c21a0682981aac9b777cb
Calm Andy <thought7878@qq.com>
2023-01-17 21:03:29 +0800

    style(Button): update structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,22 @@ function App() {
 	return (
 		<div className="App">
 			<div>
-				<Button btnType={ButtonType.Default} size={ButtonSize.Smaill}>
-					Default Smaill
-				</Button>
-				<Button btnType={ButtonType.Default}>Default</Button>
-				<Button btnType={ButtonType.Default} size={ButtonSize.Large}>
-					Default Large
-				</Button>
-			</div>
-			<div>
 				<Button btnType={ButtonType.Primary} size={ButtonSize.Smaill}>
 					Primary Smaill
 				</Button>
 				<Button btnType={ButtonType.Primary}>Primary</Button>
 				<Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
 					Primary Large
+				</Button>
+			</div>
+
+			<div>
+				<Button btnType={ButtonType.Default} size={ButtonSize.Smaill}>
+					Default Smaill
+				</Button>
+				<Button btnType={ButtonType.Default}>Default</Button>
+				<Button btnType={ButtonType.Default} size={ButtonSize.Large}>
+					Default Large
 				</Button>
 			</div>
 
@@ -34,14 +35,31 @@ function App() {
 				</Button>
 			</div>
 
-			<Button disabled>Default disabled</Button>
-			<Button
-				btnType={ButtonType.Link}
-				href="https://zh-hans.reactjs.org/docs/react-component.html#defaultprops"
-				disabled
-			>
-				Link
-			</Button>
+			<div>
+				<Button disabled size={ButtonSize.Smaill}>
+					Disabled Smaill
+				</Button>
+				<Button disabled>Disabled</Button>
+				<Button disabled size={ButtonSize.Large}>
+					Disabled Large
+				</Button>
+			</div>
+
+			<div>
+				<Button
+					btnType={ButtonType.Link}
+					href="https://zh-hans.reactjs.org/docs/react-component.html#defaultprops"
+				>
+					Link
+				</Button>
+				<Button
+					btnType={ButtonType.Link}
+					href="https://zh-hans.reactjs.org/docs/react-component.html#defaultprops"
+					disabled
+				>
+					Link
+				</Button>
+			</div>
 		</div>
 	);
 }
