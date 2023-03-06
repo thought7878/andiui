60ce9a860526d429c150ef57e9916ba30aa6e192
Calm Andy <thought7878@qq.com>
2023-03-06 13:37:30 +0800

    refactor(Progress): codes & docs

--- src/components/Progress/index.stories.tsx
+++ src/components/Progress/index.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Progress from "./";
 
 const progressMeta: ComponentMeta<typeof Progress> = {
-	title: "Progress 组件",
+	title: "Progress",
 	component: Progress,
 };
 export default progressMeta;
@@ -11,22 +11,82 @@ export default progressMeta;
 export const ProgressWithDefault: ComponentStory<typeof Progress> = (args) => {
 	return (
 		<div className="w-[500px] ">
-			<Progress progress={80} />
+			<Progress {...args} />
 		</div>
 	);
 };
-ProgressWithDefault.storyName = "设置progress和children";
+ProgressWithDefault.args = {
+	progress: 80,
+};
+ProgressWithDefault.storyName = "default";
 
 //
-export const ProgressWithCustom: ComponentStory<typeof Progress> = (args) => {
+export const ProgressWithProgress: ComponentStory<typeof Progress> = (args) => {
 	return (
 		<div className="w-[500px] ">
 			<Progress
-				progress={20}
-				outerClass="bg-yellow-500 h-[12px]"
-				innerClass="bg-Purple-500"
+				{...args}
+				// outerClass="bg-yellow-500 h-[12px]"
+				// innerClass="bg-Purple-500"
 			/>
 		</div>
 	);
 };
-ProgressWithCustom.storyName = "设置size";
+ProgressWithProgress.args = {
+	progress: 20,
+};
+ProgressWithProgress.storyName = "progress";
+
+//
+export const ProgressWithShowNumber: ComponentStory<typeof Progress> = (
+	args
+) => {
+	return (
+		<div className="w-[500px] ">
+			<Progress {...args} />
+		</div>
+	);
+};
+ProgressWithShowNumber.args = {
+	progress: 80,
+	showNumber: false,
+};
+ProgressWithShowNumber.storyName = "showNumber";
+
+//
+export const ProgressWithCustomInner: ComponentStory<typeof Progress> = (
+	args
+) => {
+	return (
+		<div className="w-[500px] ">
+			<Progress
+				{...args}
+				// outerClass="bg-yellow-500 h-[12px]"
+				innerClass="bg-purple-500"
+			/>
+		</div>
+	);
+};
+ProgressWithCustomInner.args = {
+	progress: 20,
+};
+ProgressWithCustomInner.storyName = "custom inner style";
+
+//
+export const ProgressWithCustomOuter: ComponentStory<typeof Progress> = (
+	args
+) => {
+	return (
+		<div className="w-[500px] ">
+			<Progress
+				{...args}
+				outerClass="bg-yellow-500 h-[16px]"
+				// innerClass="bg-purple-500"
+			/>
+		</div>
+	);
+};
+ProgressWithCustomOuter.args = {
+	progress: 20,
+};
+ProgressWithCustomOuter.storyName = "custom outer style";
--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react";
 
-interface ProgressProps {
-	progress: number; //0~100
-
-	/* TODO: unfinished custom class */
-	/**Set the style of outer container */
-	outerClass?: string;
+export interface ProgressProps {
+	/**progress number,0~100 */
+	progress: number;
+	/**should show number */
+	showNumber?: boolean;
 	/**Set the style of the inner container */
 	innerClass?: string;
-	// text?: string;
+	/**Set the style of outer container */
+	outerClass?: string;
 }
 
 /**
@@ -21,20 +21,22 @@ interface ProgressProps {
  *
  */
 export const Progress: FC<ProgressProps> = (props) => {
-	const { outerClass, innerClass, progress = 0 } = props;
+	const { outerClass, innerClass, showNumber, progress = 0 } = props;
 
 	return (
-		<div
-			className={`progress-outer relative h-4 overflow-hidden rounded-md bg-auiLight-border ${outerClass}`}
-		>
+		<div className={`progress-outer ${outerClass}`}>
 			<div
 				style={{ width: `${progress}%` }}
-				className={`progress-inner absolute top-0 left-0 flex h-full items-center justify-end rounded-md bg-primary text-xs text-white transition-all  ${innerClass}`}
+				className={`progress-inner ${innerClass}`}
 			>
-				{progress}%
+				{showNumber && `${progress}%`}
 			</div>
 		</div>
 	);
+};
+Progress.defaultProps = {
+	progress: 0,
+	showNumber: true,
 };
 
 export default Progress;
